fb4865bc869a774f815adbc59b39f5acb50cd842
Wilkor <wilkorguitarrista@msn.com>
2023-02-17 12:31:46 -0300

    Primeira versão
    Subindo a primeira versão

---
+++ static/js/2.a0ce2f9f.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{96:function(n,w){}}]);
+//# sourceMappingURL=2.a0ce2f9f.chunk.js.map
---
+++ static/js/49.a09ece2c.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[49],{145:function(t,n,o){"use strict";o.r(n),o.d(n,"bds_modal_action",(function(){return c}));var i=o(17),c=function(){function t(t){Object(i.r)(this,t)}return t.prototype.render=function(){return Object(i.h)("div",{class:"modal__action"},Object(i.h)("slot",null))},t}();c.style=".modal__action{display:-ms-flexbox;display:flex;padding-top:16px;bottom:32px;right:32px}"}}]);
+//# sourceMappingURL=49.a09ece2c.chunk.js.map
